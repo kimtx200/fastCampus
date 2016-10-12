957495ba1206ef8d945f293af36a7b1555cc6c55
kimtx200 <kimtx200@gmail.com>
2016-10-12 16:18:20 +0900

    #DBHelper 실습 1. DBHelper 실습 - DBHelper 생성 및 불러오기 구현

---
+++ DBHelper/app/src/main/java/com/taewonkim/android/dbhelper/DBHelper.java
@@ -0,0 +1,38 @@
+package com.taewonkim.android.dbhelper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by 태원 on 2016-10-12.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "sqlite.db";
+    public static final int DB_VERSION = 1;
+
+    private static DBHelper dbHelper = null;
+
+    private DBHelper(Context context){
+        super(context, DB_NAME, null, DB_VERSION);
+
+    }
+
+    public static SQLiteDatabase openDatabase(Context context){
+        if(dbHelper == null)
+            dbHelper = new DBHelper(context);
+
+        return dbHelper.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ DBHelper/app/src/main/java/com/taewonkim/android/dbhelper/MainActivity.java
@@ -0,0 +1,19 @@
+package com.taewonkim.android.dbhelper;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SQLiteDatabase db = DBHelper.openDatabase(this);
+
+        db.close();
+
+    }
+}
