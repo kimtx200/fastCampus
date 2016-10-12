3313665ef4d99885c1dd729c7a3977ea7599e747
kimtx200 <kimtx200@gmail.com>
2016-10-12 18:06:30 +0900

    #SecretBoard -v0.1 1. 화면구성

---
+++ SecretBoard/app/src/main/java/com/taewonkim/android/secretboard/DatabaseUtils.java
@@ -0,0 +1,8 @@
+package com.taewonkim.android.secretboard;
+
+/**
+ * Created by 태원 on 2016-10-12.
+ */
+public class DatabaseUtils {
+
+}
---
+++ SecretBoard/app/src/main/java/com/taewonkim/android/secretboard/MainActivity.java
@@ -0,0 +1,52 @@
+package com.taewonkim.android.secretboard;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager;
+
+    ArrayList<Fragment> fragments = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListFragment listFragment = new ListFragment();
+        fragments.add(listFragment);
+
+        DetailFragment detailFragment = new DetailFragment();
+        fragments.add(detailFragment);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter{
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+    }
+}
