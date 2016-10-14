e5258910bc0124359865247047706ceefae3e670
kimtx200 <kimtx200@gmail.com>
2016-10-14 11:32:08 +0900

    #Custom View -v0.2 1. onTouchEvent() 적용 - 스크린상에 터치 이벤트가 발생 했을때의 액션들을 정의 할 수 있다. - ACTION_UP 옵션을 통해 눌렀다 떼었을때 액션을 정의 할 수 있다.
    2. getX(), getY()
    - 터치 시점의 화면 좌표를 가져 온다.
    
    3. invalidate()
    - 새로운 터치가 입력 되었을 때, onDraw 를 새로이 호출해 준다.

--- ViewBasic_Custom/app/src/main/java/com/taewonkim/android/viewbasic_custom/MainActivity.java
+++ ViewBasic_Custom/app/src/main/java/com/taewonkim/android/viewbasic_custom/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,7 +32,39 @@ class CustomView extends View {
         // canvas = 붓
         // paint = 물감
         Paint paint = new Paint();
-        paint.setColor(Color.MAGENTA);
-        canvas.drawRect(0,0,300,300,paint);
+
+        // left, top, right, botton 을 순서대로 집어 넣어 준다.
+        // left 와 right 는 x좌표, top 과 bottom 은 y좌표를 나타낸다.
+//        paint.setColor(Color.MAGENTA);
+//        canvas.drawRect(0,0,300,300,paint);
+
+        paint.setColor(Color.BLUE);
+        // x좌표, y좌표, 지름
+
+        if (x >= 0 || y >= 0)
+            canvas.drawCircle(x, y, rad, paint);
+    }
+
+
+    int x = -1;
+    int y = -1;
+    int rad = 100;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            // 눌렀다가 떼었을때 동작
+            case MotionEvent.ACTION_UP:
+                // 전역 변수에 클릭한 좌표를 전달
+                x = (int)event.getX();
+                y = (int)event.getY();
+
+                // 새로운 Action 값이 들어왔을 때, draw를 새롭게 그려준다.
+                invalidate();
+
+        }
+
+
+        return super.onTouchEvent(event);
     }
 }
