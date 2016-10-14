8a8d98cefa9c66d71162b16d96ea84405ef35abc
kimtx200 <kimtx200@gmail.com>
2016-10-14 11:50:19 +0900

    #Custom View -v0.3 1. Path() 객체 생성 - 터치 액션에 대한 연속적인 움직임을 체크 할 수 있다.
    2. drawPath() 메소드 호출
    - path 객체와 paint 객체를 줌으로써 연속적으로 그림을 그릴 수 있다.

--- ViewBasic_Custom/app/src/main/java/com/taewonkim/android/viewbasic_custom/MainActivity.java
+++ ViewBasic_Custom/app/src/main/java/com/taewonkim/android/viewbasic_custom/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -21,50 +22,69 @@ public class MainActivity extends AppCompatActivity {
 
 class CustomView extends View {
 
+    int x = -1;
+    int y = -1;
+    int rad = 100;
+
+    Paint paint = new Paint();
+    Path path = new Path();
+
     public CustomView(Context context) {
         super(context);
+
+        paint.setColor(Color.MAGENTA);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(10f);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         //super.onDraw(canvas);
-
         // canvas = 붓
         // paint = 물감
-        Paint paint = new Paint();
 
         // left, top, right, botton 을 순서대로 집어 넣어 준다.
         // left 와 right 는 x좌표, top 과 bottom 은 y좌표를 나타낸다.
 //        paint.setColor(Color.MAGENTA);
 //        canvas.drawRect(0,0,300,300,paint);
 
-        paint.setColor(Color.BLUE);
-        // x좌표, y좌표, 지름
+//        paint.setColor(Color.BLUE);
+//        // x좌표, y좌표, 지름
+//
+//        if (x >= 0 || y >= 0)
+//            canvas.drawCircle(x, y, rad, paint);
 
-        if (x >= 0 || y >= 0)
-            canvas.drawCircle(x, y, rad, paint);
+        canvas.drawPath(path, paint);
     }
-
-
-    int x = -1;
-    int y = -1;
-    int rad = 100;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+
+        x = (int)event.getX();
+        y = (int)event.getY();
+
         switch (event.getAction()) {
+
             // 눌렀다가 떼었을때 동작
             case MotionEvent.ACTION_UP:
                 // 전역 변수에 클릭한 좌표를 전달
-                x = (int)event.getX();
-                y = (int)event.getY();
 
-                // 새로운 Action 값이 들어왔을 때, draw를 새롭게 그려준다.
-                invalidate();
+                break;
 
+            case MotionEvent.ACTION_DOWN:
+
+                path.moveTo(x,y);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+
+                path.lineTo(x,y);
+                break;
         }
 
-
-        return super.onTouchEvent(event);
+        // 새로운 Action 값이 들어왔을 때, draw를 새롭게 그려준다.
+        invalidate();
+        return true;
     }
 }
