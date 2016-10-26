3a6792cdd64da0572343531902766394b4fd05f3
kimtx200 <kimtx200@gmail.com>
2016-10-26 17:11:31 +0900

    #Remote_RetrofitWithOkttp-v0.1

--- Remote_RetrofitWithOkttp/app/src/main/java/com/taewonkim/android/remote_retrofitwithokttp/MainActivity.java
+++ Remote_RetrofitWithOkttp/app/src/main/java/com/taewonkim/android/remote_retrofitwithokttp/MainActivity.java
@@ -2,6 +2,13 @@ package com.taewonkim.android.remote_retrofitwithokttp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        callHttp();
+    }
+
+    public void callHttp(){
+
+        String key = "48626469646b696d3639426f657459";
+        String serviceName = "GangseoTheaterInfo";
+
+        // api 내의 데이터셋의 시작점 설정
+        int begin = 1;
+        // api 내의 데이터셋을 한번에 가져올 양을 설정 (한번에 최대 1000건까지 불러오기 가능)
+        int end = 10;
+
+        retrofit2.Call<RemoteData> remoteData = RestAdapter.getInstance().getData(key, serviceName, begin, end);
+        remoteData.enqueue(new Callback<RemoteData>() {
+            @Override
+            public void onResponse(Call<RemoteData> call, Response<RemoteData> response) {
+                if(response.isSuccessful()){
+                    RemoteData data = response.body();
+                    List<Row> rows = data.getGangseoTheaterInfo().getRow();
+                    for(Row row : rows)
+                        Log.d("TEST",row.CLG_STDT);
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RemoteData> call, Throwable t) {
+
+            }
+        });
     }
 }
--- Remote_RetrofitWithOkttp/app/src/main/java/com/taewonkim/android/remote_retrofitwithokttp/RestAdapter.java
+++ Remote_RetrofitWithOkttp/app/src/main/java/com/taewonkim/android/remote_retrofitwithokttp/RestAdapter.java
@@ -2,9 +2,21 @@ package com.taewonkim.android.remote_retrofitwithokttp;
 
 import java.net.CookieManager;
 import java.net.CookiePolicy;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.JavaNetCookieJar;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Url;
 
 /**
@@ -24,7 +36,8 @@ public class RestAdapter {
 
     public synchronized static ISeoulOpenData getInstance(){
 
-        if(service == null){
+        // OK HTTP 설정
+        if(service == null) {
             // 통신 로그를 확인하기 위한 interceptor 설정
             HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -36,11 +49,68 @@ public class RestAdapter {
             // OK HTTP 설정
             // SSL을 통한 서버 연결인 경우 인증서가 없으면 통신 자체가 안된다.
             // 이 경우, 회피하기 위해 인증서를 무시하는 세팅을 해 줄 수 있다.
+            client = configureClient(new OkHttpClient().newBuilder()) // 인증서를 무시해 주는 세팅
+                    .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+                    .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+                    .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+                    .cookieJar(new JavaNetCookieJar(manager)) // 쿠키를 저장하기위한 공간설정 - urlconnection 라이브러리가 필요하다.
+                    .addInterceptor(interceptor)            // 로그를 출력해 준다.
+                    .build();
 
-
+            // Retrofit 설정
+            service = new Retrofit.Builder()
+                    .baseUrl(SERVER_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(ISeoulOpenData.class);
         }
 
         return service;
+    }
+
+    public static OkHttpClient.Builder configureClient(final OkHttpClient.Builder builder) {
+
+        X509TrustManager x509TrustManager = new X509TrustManager() {
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            @Override
+            public void checkServerTrusted(final X509Certificate[] chain, final String authType) {
+            }
+
+            @Override
+            public void checkClientTrusted(final X509Certificate[] chain,
+                                           final String authType) {
+            }
+        };
+
+        final TrustManager[] certs = new TrustManager[]{x509TrustManager};
+
+        SSLContext ctx = null;
+        try {
+            ctx = SSLContext.getInstance("TLS");
+            ctx.init(null, certs, new SecureRandom());
+        } catch (final java.security.GeneralSecurityException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            final HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+                @Override
+                public boolean verify(final String hostname, final SSLSession session) {
+                    return true;
+                }
+            };
+
+            builder.sslSocketFactory(ctx.getSocketFactory(), x509TrustManager).hostnameVerifier(hostnameVerifier);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        return builder;
     }
 
 }
